fb159274cb8e66e2a7bc73458002e140f57da411
Himanshu <himansingh24@gmail.com>
2021-11-13 16:40:59 +0530

    Feat: Create serializers for Product and setup views

---
+++ backend_hscomputers/product/serializers.py
@@ -0,0 +1,16 @@
+from .models import Category, Product
+from rest_framework import serializers
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = (
+            "id",
+            "name",
+            "get_absolute_url",
+            "description",
+            "price",
+            "get_image",
+            "get_thumbnail",
+        )
--- backend_hscomputers/product/views.py
+++ backend_hscomputers/product/views.py
@@ -1,3 +1,12 @@
+from .models import Product
+from .serializers import ProductSerializer
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class LatestProductsList(APIView):
+    def get(self, request, format=None):
+        products = Product.objects.all()[0:4]
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)

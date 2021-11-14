0c6326b138bc9cf7aefa4ab06c713b4dfef40c4f
Himanshu <himansingh24@gmail.com>
2021-11-14 11:41:22 +0530

    Feat: Implement search functionality

--- frontend_hscomputers/src/App.vue
+++ frontend_hscomputers/src/App.vue
@@ -24,6 +24,31 @@
         id="navbar-menu"
         v-bind:class="{ 'is-active': showMobileMenu }"
       >
+        <div class="navbar-start">
+          <div class="navbar-item">
+            <form method="get" action="/search">
+              <div class="field has-addons">
+                <div class="control">
+                  <input
+                    type="text"
+                    class="input"
+                    placeholder="What are you looking for?"
+                    name="query"
+                  />
+                </div>
+
+                <div class="control">
+                  <button class="button is-success">
+                    <span class="icon">
+                      <i class="fas fa-search"></i>
+                    </span>
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+
         <div class="navbar-end">
           <router-link to="/keyboard" class="navbar-item">Keyboard</router-link>
           <router-link to="/mouse" class="navbar-item">Mouse</router-link>

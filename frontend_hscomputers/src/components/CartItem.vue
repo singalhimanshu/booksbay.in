b612c4c935ae6dde75742931682e1fdf41e340a3
Himanshu <himansingh24@gmail.com>
2021-11-14 16:15:35 +0530

    Feat: Allow users to change cart contents

--- frontend_hscomputers/src/components/CartItem.vue
+++ frontend_hscomputers/src/components/CartItem.vue
@@ -7,8 +7,10 @@
     </td>
     <td>₹{{ item.product.price }}</td>
     <td>{{ item.quantity }}</td>
+    <a @click="decrementQuantity(item)">-</a>
+    <a @click="incrementQuantity(item)">+</a>
     <td>₹{{ getItemTotal(item).toFixed(2) }}</td>
-    <td><button class="delete"></button></td>
+    <td><button class="delete" @click="removeFromCart(item)"></button></td>
   </tr>
 </template>
 
@@ -26,6 +28,24 @@ export default {
   methods: {
     getItemTotal(item) {
       return item.quantity * item.product.price;
+    },
+    decrementQuantity(item) {
+      item.quantity -= 1;
+      if (item.quantity === 0) {
+        this.$emit("removeFromCart", item);
+      }
+      this.updateCart();
+    },
+    incrementQuantity(item) {
+      item.quantity += 1;
+      this.updateCart();
+    },
+    updateCart() {
+      localStorage.setItem("cart", JSON.stringify(this.$store.state.cart));
+    },
+    removeFromCart(item) {
+      this.$emit("removeFromCart", item);
+      this.updateCart();
     },
   },
 };
--- frontend_hscomputers/src/views/Cart.vue
+++ frontend_hscomputers/src/views/Cart.vue
@@ -21,6 +21,7 @@
               v-for="item in cart.items"
               v-bind:key="item.product.id"
               v-bind:initialItem="item"
+              v-on:removeFromCart="removeFromCart"
             />
           </tbody>
         </table>
@@ -57,6 +58,13 @@ export default {
   },
   mounted() {
     this.cart = this.$store.state.cart;
+  },
+  methods: {
+    removeFromCart(item) {
+      this.cart.items = this.cart.items.filter(
+        (i) => i.product.id !== item.product.id
+      );
+    },
   },
   computed: {
     cartTotalLength() {

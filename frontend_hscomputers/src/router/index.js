e92955963b1d02fabc3aaf69808d76c0ccc750cf
Himanshu <himansingh24@gmail.com>
2021-11-16 13:02:00 +0530

    Feat: Implement My Account page
    Also change the button on navbar dynamically based on the authentication
    status of the user.

--- frontend_hscomputers/src/router/index.js
+++ frontend_hscomputers/src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
+import store from "../store";
 import Home from "../views/Home.vue";
 import Product from "../views/Product.vue";
 import Category from "../views/Category.vue";
@@ -6,6 +7,7 @@ import Search from "../views/Search.vue";
 import Cart from "../views/Cart.vue";
 import SignUp from "../views/SignUp.vue";
 import LogIn from "../views/LogIn.vue";
+import MyAccount from "../views/MyAccount.vue";
 
 const routes = [
   {
@@ -33,6 +35,14 @@ const routes = [
     component: LogIn,
   },
   {
+    path: "/my-account",
+    name: "MyAccount",
+    component: MyAccount,
+    meta: {
+      requireLogin: true,
+    },
+  },
+  {
     path: "/search",
     name: "Search",
     component: Search,
@@ -57,6 +67,17 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (
+    to.matched.some((record) => record.meta.requireLogin) &&
+    !store.state.isAuthenticated
+  ) {
+    next({ name: "LogIn", query: { to: to.path } });
+  } else {
+    next();
+  }
 });
 
 export default router;

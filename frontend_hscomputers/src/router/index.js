68620cbc5dbdf24762c0f66bc70b6323ca4ff85e
Himanshu <himansingh24@gmail.com>
2021-11-17 16:41:19 +0530

    Feat: Implement a basic success page
    This page will be visible after the user has completed the payment

--- frontend_hscomputers/src/router/index.js
+++ frontend_hscomputers/src/router/index.js
@@ -9,6 +9,7 @@ import SignUp from "../views/SignUp.vue";
 import LogIn from "../views/LogIn.vue";
 import MyAccount from "../views/MyAccount.vue";
 import Checkout from "../views/Checkout.vue";
+import Success from "../views/Success.vue";
 
 const routes = [
   {
@@ -60,6 +61,11 @@ const routes = [
     path: "/cart",
     name: "Cart",
     component: Cart,
+  },
+  {
+    path: "/cart/success",
+    name: "Success",
+    component: Success,
   },
   {
     path: "/:category_slug/:product_slug/",
